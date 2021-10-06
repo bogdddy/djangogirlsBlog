43204278802634d40a58292facc2286db6169de5
bogdan <bogdan@reusmobilitat.cat>
2021-10-06 12:56:03 +0200

    crispy forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 from .models import Post
 
@@ -8,3 +10,23 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+    title = forms.CharField(widget=forms.TextInput(attrs=({'placeholder': 'title'})))
+    text = forms.CharField(widget=forms.Textarea(attrs=({'placeholder': 'title'})))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('title', css_class='form-group col-md-8 m-2')
+            ),
+            Row(
+                Column('text', css_class='form-group col-md-8 m-2')
+            ),
+            Row(
+                Submit('submit', 'save', css_class="m-3")
+            )
+        )
+
+
--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
